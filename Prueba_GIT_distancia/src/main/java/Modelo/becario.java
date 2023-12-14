60e48bc5d75f67115385e6fbff8cc6619fda2f68
Usuario <Usuario@DESKTOP-0DILOR8>
2023-12-14 10:14:26 +0100

    Primer comit de la rama becario

---
+++ Prueba_GIT_distancia/src/main/java/Modelo/becario.java
@@ -0,0 +1,12 @@
+package Modelo;
+
+public class becario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Trabajo duro del becario");
+	}
+
+}
